edb69bcd1f4c9f06805c11d78cb06b442c059a95
Alexey Nosov <mwmaleks@gmx.com>
2013-10-07 05:47:35 -0400

    [#2] Added validation to class Position

--- models/position.js
+++ models/position.js
@@ -1,8 +1,21 @@
-var Backbone = require('backbone');
-var _ = require('underscore');
+var Backbone, _;
+
+Backbone = require('backbone');
+_ = require('underscore');
 
 var Position = Backbone.Model.extend({
+    
+    validate: function(attrs, options) {
 
+       if ( !_.isString(attrs.name) ) {
+         return "Наименование позиции должно быть строкой";
+       }
+       
+       if ( !_.isNumber(attrs.price) ) {
+           return "Велечина оклада должна быть указана числом";
+       }
+    }
 });
 
 module.exports = Position;
+
